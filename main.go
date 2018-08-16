abbb35998ea0e48f577e26f4a7554f6a19af4b76
su-kun1899 <higedrum.coz@gmail.com>
2018-08-16 20:24:38 +0900

    add urfave/cli

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"github.com/urfave/cli"
+	"fmt"
+	"os"
+	"log"
+)
+
 func main() {
-	println("Hello, world")
+	app := cli.NewApp()
+	app.Name = "shomu2"
+	app.Usage = "fight the loneliness!"
+	app.Version = "0.0.1"
+	app.Action = func(c *cli.Context) error {
+		fmt.Println("Hello friend!")
+		return nil
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
