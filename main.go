fe8ddee0c92da383f88c5d609594fced81e68039
su-kun1899 <higedrum.coz@gmail.com>
2018-09-23 17:50:39 +0900

    コマンド分岐の骨格を作った

--- main.go
+++ main.go
@@ -6,25 +6,22 @@ import (
 )
 
 func runCmd(args []string) int {
-	fmt.Println("Hello friend!")
-
 	// 引数のチェック
-	//if len(args) != 2 {
-	//	fmt.Fprintf(os.Stderr, "[ERROR] required two arguments(pattern,package)\n")
-	//	return 1
-	//}
-	//pattern := args[0]
-	//packageName := args[1]
-	//
-	//comments, err := comment.ExtractComments(pattern, packageName)
-	//if err != nil {
-	//	fmt.Fprintf(os.Stderr, "[ERROR] %v\n", err)
-	//	return 1
-	//}
-	//
-	//for _, c := range comments {
-	//	fmt.Printf(c.String())
-	//}
+	if len(args) != 2 {
+		fmt.Fprintf(os.Stderr, "[ERROR] required two arguments(command,item)\n")
+		return 1
+	}
+
+	command := args[0]
+	item := args[1]
+	switch command {
+	case "add":
+		// TODO
+		fmt.Fprintf(os.Stdout, "item: %v を追加するよ\n", item)
+	default:
+		fmt.Fprintf(os.Stderr, "[ERROR] not exist command: %s \n", command)
+		return 1
+	}
 
 	return 0
 }
