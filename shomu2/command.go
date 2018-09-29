3424d82ada316c0d4bd303803ab128c256abaeb7
su-kun1899 <higedrum.coz@gmail.com>
2018-09-29 23:34:02 +0900

    コマンドが存在しない場合はエラー

--- shomu2/command.go
+++ shomu2/command.go
@@ -1,5 +1,10 @@
 package shomu2
 
+import (
+	"errors"
+	"fmt"
+)
+
 type Command interface {
 	Run(options ...string)
 }
@@ -11,6 +16,11 @@ func (*Push) Run(options ...string) {
 	panic("implement me")
 }
 
-func NewCommand(name string)(Command, error) {
-	return &Push{}, nil
+func NewCommand(name string) (Command, error) {
+	switch name {
+	case "push":
+		return &Push{}, nil
+	default:
+		return nil, errors.New(fmt.Sprintf("command \"%s\" is not exist", name))
+	}
 }
--- shomu2/command_test.go
+++ shomu2/command_test.go
@@ -5,15 +5,25 @@ import (
 	"github.com/su-kun1899/shomu2/shomu2"
 )
 
-func TestNewCommand(t *testing.T) {
-	// when
-	command, err := shomu2.NewCommand("add")
-	if err != nil {
-		t.Error("unexpected error:", err)
-	}
+func TestNewCommand_push(t *testing.T) {
+	t.Run("create push command", func(t *testing.T) {
+		// when
+		command, err := shomu2.NewCommand("push")
+		if err != nil {
+			t.Error("unexpected error:", err)
+		}
 
-	// then
-	if _, ok := command.(*shomu2.Push); !ok {
-		t.Errorf("command is not push command: %v", command)
-	}
+		// then
+		if _, ok := command.(*shomu2.Push); !ok {
+			t.Errorf("command is not push command: %v", command)
+		}
+	})
+
+	t.Run("not exists command", func(t *testing.T) {
+		// when-then
+		_, err := shomu2.NewCommand("foo")
+		if err == nil {
+			t.Error("expected error did not occur")
+		}
+	})
 }
