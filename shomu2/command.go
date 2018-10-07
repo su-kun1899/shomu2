927624993c88f238d722230745484047efb3310f
su-kun1899 <higedrum.coz@gmail.com>
2018-10-07 15:55:40 +0900

    RepositoryをMockしてテストしてみる

--- shomu2/command.go
+++ shomu2/command.go
@@ -10,16 +10,17 @@ type Command interface {
 }
 
 type Push struct {
+	repository Repository
 }
 
-func (*Push) Run(options ...string) {
-	panic("implement me")
+func (command *Push) Run(options ...string) {
+	command.repository.Add(Item{Value: options[0]})
 }
 
-func NewCommand(name string) (Command, error) {
+func NewCommand(name string, repository Repository) (Command, error) {
 	switch name {
 	case "push":
-		return &Push{}, nil
+		return &Push{repository: repository}, nil
 	default:
 		return nil, errors.New(fmt.Sprintf("command \"%s\" is not exist", name))
 	}
--- shomu2/command_test.go
+++ shomu2/command_test.go
@@ -8,7 +8,7 @@ import (
 func TestNewCommand_push(t *testing.T) {
 	t.Run("create push command", func(t *testing.T) {
 		// when
-		command, err := shomu2.NewCommand("push")
+		command, err := shomu2.NewCommand("push", &fakeRepository{})
 		if err != nil {
 			t.Error("unexpected error:", err)
 		}
@@ -21,9 +21,42 @@ func TestNewCommand_push(t *testing.T) {
 
 	t.Run("not exists command", func(t *testing.T) {
 		// when-then
-		_, err := shomu2.NewCommand("foo")
+		_, err := shomu2.NewCommand("foo", &fakeRepository{})
 		if err == nil {
 			t.Error("expected error did not occur")
 		}
 	})
+}
+
+func TestPush_Run(t *testing.T) {
+	// given
+	called := false
+	repository := fakeRepository{fakeAdd: func(item shomu2.Item) error {
+		called = true
+		return nil
+	}}
+	command, err := shomu2.NewCommand("push", &repository)
+	if err != nil {
+		t.Fatal("unexpected error:", err)
+	}
+
+	// and
+	item := shomu2.Item{Value: "push!push!"}
+
+	// when
+	command.Run(item.Value)
+
+	// then
+	if !called {
+		t.Error("Repository is not called")
+	}
+}
+
+type fakeRepository struct {
+	shomu2.Repository
+	fakeAdd func(item shomu2.Item) error
+}
+
+func (repository *fakeRepository) Add(item shomu2.Item) error {
+	return repository.fakeAdd(item)
 }
