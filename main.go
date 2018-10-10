87a63354ce65969b67ade64841bedefe7fb1ca88
su-kun1899 <higedrum.coz@gmail.com>
2018-10-10 20:35:53 +0900

    環境変数からファイルの置き場所を決める

--- main.go
+++ main.go
@@ -3,20 +3,29 @@ package main
 import (
 	"github.com/su-kun1899/shomu2/shomu2"
 	"os"
+	"path/filepath"
 )
 
 // TODO こいつ自体も場所変えていいのかも。。
 func runCmd(args []string) shomu2.ExitStatus {
 	commandType := args[0]
 	// TODO ファイルの場所をユーザーディレクトリとかにする？
-	repository, err := shomu2.NewRepository("sample")
+	homeDir := os.Getenv("SHOMU2_HOME")
+	if homeDir == "" {
+		// TODO
+		panic("SHOMU2_HOMEが未設定")
+	}
+
+	repository, err := shomu2.NewRepository(homeDir + filepath.FromSlash("/data"))
 	if err != nil {
 		// TODO
+		panic(err)
 	}
 
 	command, err := shomu2.NewCommand(commandType, repository)
 	if err != nil {
 		// TODO
+		panic(err)
 	}
 
 	// TODO 可変長引数じゃなくて、配列にしたほうがよさげ
