ad6a5e38745f517c0b84d48a5fa0949f3a33f112
su-kun1899 <higedrum.coz@gmail.com>
2018-10-13 16:35:30 +0900

    Remove duplicated test

--- shomu2/command_test.go
+++ shomu2/command_test.go
@@ -31,66 +31,6 @@ func TestNewCommand_push(t *testing.T) {
 	})
 }
 
-func TestPush_Run1(t *testing.T) {
-	// TODO テーブルテストにできる？
-	t.Run("Run push command", func(t *testing.T) {
-		// given
-		param := shomu2.Item{Value: "push!push!"}
-
-		// and: mocking repository
-		called := false
-		repository := fakeRepository{fakeAdd: func(item shomu2.Item) error {
-			called = reflect.DeepEqual(param, item)
-			return nil
-		}}
-
-		// and: create command
-		command, err := shomu2.NewCommand("push", &repository)
-		if err != nil {
-			t.Fatal("unexpected error:", err)
-		}
-
-		// when
-		exitStatus := command.Run([]string{param.Value})
-
-		// then
-		if exitStatus.Code != shomu2.Success {
-			t.Errorf("Run want %v but got %v", shomu2.Success, exitStatus.Code)
-		}
-		if !called {
-			t.Error("Repository is not called")
-		}
-	})
-
-	t.Run("Push command args error", func(t *testing.T) {
-		// given: mocking repository
-		repository := fakeRepository{fakeAdd: func(item shomu2.Item) error { return nil }}
-
-		// and: create command
-		command, err := shomu2.NewCommand("push", &repository)
-		if err != nil {
-			t.Fatal("unexpected error:", err)
-		}
-
-		// when
-		exitStatus := command.Run(nil)
-
-		// then
-		if exitStatus.Code != shomu2.Fail {
-			t.Errorf("Run want %v but got %v", shomu2.Fail, exitStatus.Code)
-		}
-	})
-}
-
-type fakeRepository struct {
-	shomu2.Repository
-	fakeAdd func(item shomu2.Item) error
-}
-
-func (repository *fakeRepository) Add(item shomu2.Item) error {
-	return repository.fakeAdd(item)
-}
-
 func TestPush_Run(t *testing.T) {
 	callRepository := false
 	type fields struct {
@@ -176,4 +116,14 @@ func TestPush_Run(t *testing.T) {
 			}
 		})
 	}
+}
+
+
+type fakeRepository struct {
+	shomu2.Repository
+	fakeAdd func(item shomu2.Item) error
+}
+
+func (repository *fakeRepository) Add(item shomu2.Item) error {
+	return repository.fakeAdd(item)
 }
