7786eae24545a121b7e38538e934e7190a7d43eb
su-kun1899 <higedrum.coz@gmail.com>
2018-12-13 23:41:15 +0900

    Refactor test

--- shomu2/command_test.go
+++ shomu2/command_test.go
@@ -23,6 +23,11 @@ func TestNewCommand(t *testing.T) {
 			args:     args{cmdName: "push", repository: &fakeRepository{}},
 			wantType: reflect.TypeOf(&shomu2.Push{}),
 		},
+		{
+			name:     "create pop command",
+			args:     args{cmdName: "pop", repository: &fakeRepository{}},
+			wantType: reflect.TypeOf(&shomu2.Pop{}),
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -45,21 +50,6 @@ func TestNewCommand_Push(t *testing.T) {
 		_, err := shomu2.NewCommand("foo", &fakeRepository{})
 		if err == nil {
 			t.Error("expected error did not occur")
-		}
-	})
-}
-
-func TestNewCommand_Pop(t *testing.T) {
-	t.Run("create pop command", func(t *testing.T) {
-		// when
-		command, err := shomu2.NewCommand("pop", &fakeRepository{})
-		if err != nil {
-			t.Fatal("unexpected error:", err)
-		}
-
-		// then
-		if _, ok := command.(*shomu2.Pop); !ok {
-			t.Errorf("command is %v, want %s", reflect.ValueOf(command).Type(), "*shomu2.Pop")
 		}
 	})
 }
